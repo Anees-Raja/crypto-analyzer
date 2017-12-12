91b7596df06a47faec05c55cbc26b02503ca9347
Anees Raja <Anees@degnon.org>
2017-12-12 11:50:09 -0500

    adding coinList + displaying stuff

--- src/App.js
+++ src/App.js
@@ -1,33 +1,57 @@
 import React, { Component } from 'react';
 import './App.css';
 import cc from 'cryptocompare';
+var NumberFormat = require('react-number-format');
 
 class App extends Component {
-  
+
   constructor(props) {
     super(props);
 
     this.state = {
       coins: [],
+      listOfCoins: []
     };
+
   }
-
-  
-
   componentDidMount() {
-    cc.priceMulti(['BTC', 'LTC', 'IOT'], ['USD'])
+    
+    cc.coinList()
       .then(res => {
-        this.setState({ coins: res });
-        console.log(this.state)
+        var listOfCoins = res.Data;
+        console.log(listOfCoins);
+        this.setState({
+          listOfCoins: listOfCoins
+        });
       })
       .catch(console.error)
-  }
 
+    cc.priceMulti(['BTC', 'LTC', 'IOT'], ['USD'])
+      .then(res => {
+        const coins = res;
+        this.setState({ coins: coins });
+        console.log(res)
+      })
+      .catch(console.error)
+
+    }
 
   render() {
     return (
       <div className="App">
+        {Object.keys(this.state.coins).map(key => (
+          <div id="coin-container">
+            <span className="left">{key}</span>
+            <span className="right">
+            <NumberFormat 
+            value={this.state.coins[key].USD} 
+            displayType={'text'}
+            prefix={'$'}
+            decimal={2}
+            thousandSeparator={true} /></span>
+          </div>
 
+        ))}
       </div>
     );
   }
